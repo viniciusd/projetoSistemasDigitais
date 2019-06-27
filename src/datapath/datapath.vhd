fc7685536b42906d794cc2913c9dbb291d6dde89
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2019-06-27 16:12:05 -0300

    Fix ALU's output type at the datapath port mapping

--- src/datapath/datapath.vhd
+++ src/datapath/datapath.vhd
@@ -178,7 +178,7 @@ ARCHITECTURE datapath_arch OF datapath IS
 
     SIGNAL reg_mux_out_A_output : std_logic_vector (7 downto 0);
     SIGNAL reg_mux_out_B_output : std_logic_vector (7 downto 0);
-    SIGNAL ALU_OUT              : std_logic_vector (7 downto 0);
+    SIGNAL ALU_OUT              : signed (7 downto 0);
     SIGNAL alu_carryout         : std_logic;
 
     SIGNAL reg_in_output        : std_logic_vector (7 downto 0);
@@ -190,7 +190,7 @@ BEGIN
     pc_mux         : mux2_1          PORT MAP (instruction(10 downto 0), stack_value, pc_switch, I_PC);
     program_memory : single_port_rom PORT MAP (clock, to_integer(unsigned(O_PC)), program_data);
     data_memory    : single_port_ram PORT MAP (clock, to_integer(unsigned(instruction(7 downto 0))), reg_mux_out_A_output, D_wr, data_memory_rd);
-    reg_mux_in     : mux8_1          PORT MAP (ALU_OUT, data_memory_rd, instruction(7 downto 0),
+    reg_mux_in     : mux8_1          PORT MAP (std_logic_vector(ALU_OUT), data_memory_rd, instruction(7 downto 0),
                                                reg_mux_out_A_output, reg_in_output,
                                                "00000000", "00000000", "00000000",
                                                register_file_switch, reg_input);
